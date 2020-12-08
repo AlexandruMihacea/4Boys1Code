b39e606d93939f4283373e13ab7c0ebb4e83009d
daniiiii3303 <72355145+daniiiii3303@users.noreply.github.com>
2020-12-08 22:49:05 +0200

    Continuam

--- primaIncercareDeCod.cpp
+++ primaIncercareDeCod.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 class coloana_int
 {
 	int* x;
 	string nume_coloana;
-	int nr_inregistrari;
+	
 	~coloana_int()
 	{
 		delete[] x;
@@ -14,7 +15,7 @@ class coloana_string
 {
 	string* y;
 	string nume_coloana;
-	int nr_inregistrari;
+	
 	~coloana_string()
 	{
 		delete[] y;
@@ -24,8 +25,8 @@ class coloana_date
 {
 	time_t* data;
 	string nume_coloana;
-	int nr_inregistrari;
-	coloana_date()
+	
+	~coloana_date()
 	{
 		delete[] data;
 	}
@@ -43,28 +44,55 @@ class tabela
 	int nr_atribute_intregi;              // intregi[0].x[0] = 18; - prima varsta din coloana VARSTE
 	coloana_string** stringuri;           // intregi[0].x[1] = 19;
 	int nr_atribute_string;               // intregi[1].nume =  "NOTE";
-	coloana_date** date;                  // intregi[1].x[0] = 10; - prima nota din coloana NOTE
+	/*coloana_date** date; */                 // intregi[1].x[0] = 10; - prima nota din coloana NOTE
 	int nr_atribute_date;
+	int nr_inregistrari;
 	
 
 };
+string* removeDupWord(string str, string* cuvinte, int& marime)
+{
+	string word = "";
+	for (int i=0;i<str.length(); i++)
+	{
+		if ((str[i] == ' ' || str[i] == '(' || str[i] == ')' || str[i] == ',') &&(str[i+1]  == ' ' || str[i+1] == '(' || str[i+1]  == ')' || str[i+1]  == ','))
+		{
+			continue;
+		}
+		if ((str[i] == ' ' || str[i] == '(' || str[i] == ')' || str[i] == ',') && (str[i + 1]  != ' ' || str[i+1] != '(' || str[i+1] != ')' || str[i+1]  != ','))
+		{
+			marime += 1;
+			word = "";
+		}
+		else 
+		{
+			word = word + str[i];
+			cuvinte[marime] = word;
+		}
+		
+	}
+	
+	return cuvinte;
 
-
+}
 //create table student (  (nume, string,  3, ""), (varsta, int, 3, 1)   )
-
 int main()
 {
-	// create table abecedar
-	// select * from abecedar
-	// tabela abecedar; 
-	/*f >> string; 
-	if (string == "select * from abecedar")
+	string x;
+	getline(cin, x);
+	
+	int i = 0;
+	string* cuvinte=nullptr; 
+	cuvinte = new string[100];
+	removeDupWord(x, cuvinte,i);
+	//cuvinte = new string[i];
+	cout << cuvinte->length();
+	for(int k=0;k<i;k++)
 	{
-		asdjasdjas
-	}*/
-//	string s;
-//	if (firstword == "create")
-//	{
-//		tabela farmacie;
-//	}
+		cout << cuvinte[k]<<endl;
+	}
+	
+	delete[] cuvinte;
+
+
 }
